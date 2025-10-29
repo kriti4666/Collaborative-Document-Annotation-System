113944ac98ea820b84f8b212a2c63bb3838a20dc
kriti kumari <kritikashyap4666@gmail.com>
2025-10-29 11:57:01 +0530

    modified the base url of frontend and backend

--- Annotation-system/Annotation-system/client/src/service/api.js
+++ Annotation-system/Annotation-system/client/src/service/api.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+const API_BASE_URL = import.meta.env.BASE_URL;
+console.log("base url:", API_BASE_URL)
+
 const API = axios.create({
-    baseURL: 'http://localhost:8000',
+    // baseURL: 'http://localhost:8000',
+    baseURL: API_BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 export default API;
--- Annotation-system/Annotation-system/client/src/utils/socket.js
+++ Annotation-system/Annotation-system/client/src/utils/socket.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:8000');
+const socket = io('https://doc-annotation-backend.onrender.com/');
 
 export const useSocket = () => {
     const [isConnected, setIsConnected] = useState(false);
